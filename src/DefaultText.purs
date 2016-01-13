9511cb1cd3c13df1efbd50284a277890e73d67e4
Péter Diviánszky <divipp@gmail.com>
2016-01-14 00:28:32 +0100

    change default text

--- src/DefaultText.purs
+++ src/DefaultText.purs
@@ -1,23 +1,19 @@
 module DefaultText where
 
 defaultSrc = """
-clear = FrameBuffer $ (DepthImage @1 1000, ColorImage @1 navy)   -- ...
+makeFrame (projmat :: Mat 4 4 Float)
+          (vertexstream :: VertexStream Triangle (Vec 4 Float))
 
-triangleRasterCtx = TriangleCtx CullNone PolygonFill NoOffset LastVertex
-colorFragmentCtx = AccumulationContext (DepthOp Less True, ColorOp NoBlending (V4 True True True True))
+    = imageFrame (emptyDepthImage 1000, emptyColorImage navy)
+  `overlay`
+      vertexstream
+    & transformVertices (scale 0.5 . (projmat *.))
+    & rasterize (TriangleCtx CullNone PolygonFill NoOffset LastVertex)
+    & filterFragmentStream PassAll
+    & transformFragmentsRastDepth (\x -> x)
+    & accumulateWith (DepthOp Less True, ColorOp NoBlending (V4 True True True True))
 
-rasterizeWith = Rasterize
-triangles = triangleRasterCtx
-
-cubeVertexStream = Fetch "stream4" Triangles (Attribute "position4" :: Vec 4 Float)
-mapFragments s fs = Accumulate colorFragmentCtx PassAll (\a -> FragmentOutRastDepth $ fs a) s clear
-transform s f =  Transform (\v -> VertexOut (f v) 1 () (Smooth v)) s
-
-rotate' v = (Uniform "MVP" :: Mat 4 4 Float) *. v
-
-main =             cubeVertexStream         -- cube vertices
-    `transform`    (scale 0.5 . rotate')    -- scale them
-     &             rasterizeWith triangles  -- rasterize
-    `mapFragments` id
-     &             ScreenOut                --  draw into screen
+main = renderFrame $
+   makeFrame (Uniform "MVP")
+             (Fetch "stream4" (Attribute "position4"))
 """
