4cc3da72fbf55e0bbe38e3c13e6eca4bc36f8c44
Péter Diviánszky <divipp@gmail.com>
2016-01-14 11:50:10 +0100

    update default text

--- src/DefaultText.purs
+++ src/DefaultText.purs
@@ -1,7 +1,11 @@
 module DefaultText where
 
 defaultSrc = """
-makeFrame (projmat :: Mat 4 4 Float)
+
+f x = (x + sin x + sin (1.1 * x)) `mod` 4.0 * 2
+
+makeFrame (time :: Float)
+          (projmat :: Mat 4 4 Float)
           (vertexstream :: VertexStream Triangle (Vec 4 Float))
 
     = imageFrame (emptyDepthImage 1000, emptyColorImage navy)
@@ -10,10 +14,12 @@ makeFrame (projmat :: Mat 4 4 Float)
     & transformVertices (scale 0.5 . (projmat *.))
     & rasterize (TriangleCtx CullNone PolygonFill NoOffset LastVertex)
     & filterFragmentStream PassAll
-    & transformFragmentsRastDepth (\x -> x)
+    & transformFragmentsRastDepth (\x -> (rotMatrixZ time *. rotMatrixY time *. x) *! f time)
     & accumulateWith (DepthOp Less True, ColorOp NoBlending (V4 True True True True))
 
 main = renderFrame $
-   makeFrame (Uniform "MVP")
+   makeFrame (Uniform "Time")
+             (Uniform "MVP")
              (Fetch "stream4" (Attribute "position4"))
+
 """
