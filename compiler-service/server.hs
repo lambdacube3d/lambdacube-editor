527fa2cfa8098194cf0605f24d428547e42dfe46
Péter Diviánszky <divipp@gmail.com>
2016-04-27 07:16:51 +0200

    refactoring

--- compiler-service/server.hs
+++ compiler-service/server.hs
@@ -1,123 +1,97 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-
---------------------------------------------------------------------------------
-import Data.Maybe
-import           Control.Concurrent      (forkIO)
-import           Control.Exception       (finally)
-import           Control.Monad           (forever, unless)
+import           Data.List               (sort)
+import qualified Data.Vector             as V
+import           Control.Monad           (forever, when)
 import qualified Data.ByteString         as B
 import qualified Data.ByteString.Char8   as BC
-import qualified Data.ByteString.Lazy    as BL
+import           Data.Aeson
+import           Data.Aeson.Encode.Pretty
+import qualified System.IO               as IO
+import           System.Directory
 import qualified Network.WebSockets      as WS
 import qualified Network.WebSockets.Snap as WS
-import           Snap.Core               (Snap)
 import qualified Snap.Core               as Snap
 import qualified Snap.Http.Server        as Snap
-import qualified Snap.Util.FileServe     as Snap
-import qualified System.IO               as IO
-import qualified System.Process          as Process
+import qualified Snap.Snaplet.Config     as Snap
 
-import qualified Data.Vector as V
-import Data.List (sort)
-import Snap.Snaplet.Config
-import System.IO
-import System.Directory
---import Control.Exception
---import Control.Monad.IO.Class
---import Control.DeepSeq
-import Text.Show.Pretty (ppShow)
+import LambdaCube.Compiler as C
 
-import Text.Megaparsec.Pos
-import Data.Aeson.Encode.Pretty
-import Data.Aeson
 import TypeInfo as T
-import LambdaCube.Compiler as C hiding (ppShow)
-
 import Cache
 import Hash
 
 --------------------------------------------------------------------------------
+
+main :: IO ()
+main = do
+  IO.hSetBuffering IO.stdout IO.NoBuffering
+  IO.hSetBuffering IO.stdin IO.NoBuffering
+  config <- Snap.commandLineAppConfig Snap.defaultConfig
+  compiler <- preCompile [] ["exercises"] WebGL1 "Prelude.lc"
+  ch <- newCache 10
+  Snap.httpServe config $ app compiler ch
+
 --runApp x = WS.runWebSocketsSnapWith (WS.ConnectionOptions $ putStrLn "pong received") x
 runApp x = WS.runWebSocketsSnap x
 
 --app :: (String -> IO (Either String Pipeline, Infos)) -> Snap ()
 app compiler ch = Snap.route
-    [ (,) "compile" $ runApp compileApp
+    [ (,) "compile"      $ runApp compileApp
     , (,) "exerciselist" $ runApp exerciselist
-    , (,) "getexercise" $ runApp getexercise
+    , (,) "getexercise"  $ runApp getexercise
     ]
   where
 
     exerciselist :: WS.ServerApp
     exerciselist pending = do
         c <- WS.acceptRequest pending
-        let go = do
+        forever $ do
               (_ :: BC.ByteString) <- WS.receiveData c
               list <- sort . filter (`notElem` [".",".."]) <$> getDirectoryContents "exercises"
               print list
               WS.sendTextData c $ encodePretty $ toJSON list
-              go
-        go
 
     getexercise :: WS.ServerApp
     getexercise pending = do
         c <- WS.acceptRequest pending
-        let go = do
-              (name :: BC.ByteString) <- WS.receiveData c
+        forever $ do
+              name <- WS.receiveData c
               let fname = "exercises/" ++ BC.unpack name
               b <- doesFileExist fname
-              if b then do
+              when b $ do
                     src <- readFile fname
                     --print fname
                     WS.sendTextData c $ encodePretty $ toJSON src
-                else return ()
-              go
-        go
 
     compileApp :: WS.ServerApp
     compileApp pending = do
-        print "compileApp"
+        putStrLn "compileApp"
         c <- WS.acceptRequest pending
         --WS.forkPingThread c 5 
-        let go = do
+        forever $ do
               WS.sendPing c ("hello" :: B.ByteString)
               bs <- BC.unpack <$> WS.receiveData c
               --print bs
               let h = mkHash bs
               r <- lookupCache ch h
               json <- case r of
+                Left json -> return json
                 Right add -> do
                   json <- catchErr er $ encodePretty . ff <$> compiler bs
                   add json
                   return json
-                Left x -> return x
               WS.sendTextData c json
-              go
-        go
-        putStrLn $ "compileApp ended"
+        putStrLn "compileApp ended"
       where
         cvtRange (C.Range _ (SPos r c) (SPos r' c')) = T.Range r c r' c'
 
-        ff (Left err, (infos, _)) = CompileError (V.fromList eloc) err $ convertInfos infos
-          where
-            eloc = map cvtRange $ errorRange infos
+        ff (Left err, (infos, _))    = CompileError (V.fromList $ cvtRange <$> errorRange infos) err $ convertInfos infos
         ff (Right ppl, (infos, dsg)) = Compiled dsg (prettyShowUnlines ppl) ppl $ convertInfos infos
 
         er e = return $ encodePretty $ CompileError mempty ("\n!FAIL\n" ++ e) mempty
 
         convertInfos is = V.fromList [TypeInfo (cvtRange r) $ unlines c | (r, c) <- listTypeInfos is ]
-
---------------------------------------------------------------------------------
-main :: IO ()
-main = do
-  IO.hSetBuffering IO.stdout IO.NoBuffering
-  IO.hSetBuffering IO.stdin IO.NoBuffering
-  config <- commandLineAppConfig Snap.defaultConfig
-  compiler <- preCompile [] ["exercises"] WebGL1 "Prelude.lc"
-  ch <- newCache 10
-  Snap.httpServe config $ app compiler ch
 
